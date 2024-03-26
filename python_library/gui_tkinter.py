3e0d772b7e76373eab3d749c4dfe82a91e8524b8
Shadi <32618348+shadisharba@users.noreply.github.com>
2024-03-26 10:03:10 +0100

    simple GUI message box
    will be used later to test GUIs inside docker

---
+++ python_library/gui_tkinter.py
@@ -0,0 +1,19 @@
+from tkinter import messagebox as mb
+import tkinter as tk
+
+
+def start_gui():
+    """Start the GUI application."""
+    root = tk.Tk()
+    root.overrideredirect(1)
+    root.withdraw()
+    mb.showinfo('Test Message',
+                'Everything works! \n This is a test message from '
+                'python_ci_cd_demo!')
+    version_num = 0.1
+    print('The version of the called programm is ' + str(version_num))
+    root.destroy()
+
+
+if __name__ == '__main__':
+    start_gui()
