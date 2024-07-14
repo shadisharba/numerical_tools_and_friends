359638666dbffc3de7c558cd2b13d69bf63619ca
Shadi Sharba <shadisharba@gmail.com>
2024-07-14 10:30:00 +0200

    update conda env on linux

--- python_create_conda_env.bat
+++ python_create_conda_env.bat
@@ -1,1 +1,6 @@
 conda env export --no-builds> python_env_conda.yml && pip freeze > python_env_pip.txt
+
+@REM conda create --name genAI python==3.12
+@REM conda activate genAI
+@REM pip install -r python_env_pip.txt
+@REM conda env create -f python_env_conda.yml
